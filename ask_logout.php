8343946b7964c1e77015e694de3bc4a1cd075395
CheeGuo <cheeguo11@gmail.com>
2025-12-14 17:39:23 +0800

    logout function and design done
    should remove button outline , will update later .

---
+++ ask_logout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Arngren</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body class="login">
+    <div class="login-container">
+  <div class="login-card">
+    <div class="login-title">
+      <img src="pic/arngren logo.png">
+      <span>Do you wish to logout?</span>
+    </div>
+<div style="display:flex; flex-direction:column; gap:16px; align-items:center;">
+  <a href="index.php" style =  outline: none; class="login-btn">Yes</a> 
+  <a href="index.php" style =  outline: none; class="login-btn">No</a>
+</div>
+  </div>
+</div>
+
+<footer>
+    <img src="pic/footer.png" alt="footer image">
+</footer>
+</body>
+</html>
