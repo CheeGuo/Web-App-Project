9137883884a8d131702316ffde7a17dceae174eb
CheeGuo <cheeguo11@gmail.com>
2025-12-22 00:10:21 +0800

    frontend index css done

--- index.php
+++ index.php
@@ -58,10 +58,60 @@
     <h6>Robot</h6>
   </div>
 </section>
+<section class="promo-hot">
+  <div class="promo-left">
+    <img src="pic/CNY.png" alt="CNY Promotion">
+  </div>
+
+  <div class="promo-right">
+    <h3>Hot Items</h3>
+
+    <div class="hot-grid">
+      <div class="hot-card">
+        <img src="pic/dino.png">
+        <p>Dinosaur Pleo</p>
+        <span>2,999 kr</span>
+      </div>
+
+      <div class="hot-card">
+        <img src="pic/dino.png">
+        <p>4WD Land Rover Defender</p>
+        <span>4,398 kr</span>
+      </div>
+
+      <div class="hot-card">
+        <img src="pic/dino.png">
+        <p>3-Wheel e-bike</p>
+        <span>15,998 kr</span>
+      </div>
+    </div>
+  </div>
+</section>
+
 
   </main>
 
   <footer>
+    <section class="footer-info">
+  <div class="info-left">
+    <h4>Address & Company Info</h4>
+    <p>Arngren Sky Commuter AS</p>
+    <p>Att. Frithjof Arngren (Siv.ing.)</p>
+    <p>Sondreveien 3 K,</p>
+    <p>0378 Oslo, Norway.</p>
+    <p>Org.nr.: 965175113 mva</p>
+  </div>
+
+  <div class="info-divider"></div>
+
+  <div class="info-right">
+    <h4>Contact Details</h4>
+    <p>frithjof.arngren@gmail.com</p>
+    <p>Tel.: 47-22149166</p>
+    <p>Mob: 47-92060873</p>
+  </div>
+</section>
+
     <img src="pic/footer.png" alt="Footer Image">
   </footer>
 
